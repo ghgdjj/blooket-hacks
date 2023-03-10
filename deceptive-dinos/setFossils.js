6ee3e8b1bae14e111d914a15f70009f27ffb75f1
ghgdjj <127455549+ghgdjj@users.noreply.github.com>
2023-03-09 18:16:29 -0600

    Update setFossils.js

--- deceptive-dinos/setFossils.js
+++ deceptive-dinos/setFossils.js
@@ -9,7 +9,7 @@
                     return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner;
                 };
 
-                let fossils = Number(parseFloat(prompt('How many fossils do you want?')));
+                let fossils = 1,000,000(parseFloat(prompt('How many fossils do you want?')));
                 reactHandler().stateNode.setState({ fossils: fossils });
 
                 alert('Fossils added!');
